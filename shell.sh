022611d6be541ff9cbea0f30f087acefc1ae11e1
srikar5150 <60686047+srikar5150@users.noreply.github.com>
2020-02-18 13:02:27 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -25,5 +25,6 @@ do
 	done 
 done
 cd $JENKINS_HOME/workspace/$JOB_NAME/
-export PATH=/usr/local/bin
+export PATH=/usr/local/bin/
+export PATH=/usr/bin/
 eslint  -f json -c $JENKINS_HOME/workspace/eslintrc.json static_code1/*  -o $JENKINS_HOME/workspace/$JOB_NAME/result.json || echo "lint failed but continiung the process"
